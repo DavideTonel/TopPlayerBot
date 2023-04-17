d9be6286117a0a6d658971a271f2d247c62e2a2f
Tassinari Leonardo <leonardo.tassinari6@studio.unibo.it>
2023-04-18 01:13:28 +0200

    Add support for matchesLeft, and password

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import telebot
 import logging
 from config import TOKEN
+from config import PASSWORD
+import itertools
 
 from telebot import types
 
@@ -12,91 +14,144 @@ from soccer.Match import MatchBuilder
 
 tournament = Tournament()
 bot = telebot.TeleBot(TOKEN)
+whiteList = []
+if __name__ == "__main__":
+    try:
+        wlFile = open("whitelist.txt", "r")
+        ids = wlFile.readlines()
+        ids = [id.strip() for id in ids]
+        whiteList = ids
 
-@bot.message_handler(commands=['start'])
+    except Exception as e:
+        with open("whitelist.txt", "x") as f:
+            f.close
+
+
+@bot.message_handler(commands=["start"])
 def store_players(message):
     chat_id = message.chat.id
-    msg = bot.send_message(chat_id, 'Chi gioca?')
-    bot.register_next_step_handler(msg, process_names)
+    if checkPassword(chat_id):
+        msg = bot.send_message(chat_id, "Chi gioca?")
+        bot.register_next_step_handler(msg, process_names)
+
+
+def checkPassword(chat_id):
+    if str(chat_id) not in whiteList:
+        msg = bot.send_message(
+            chat_id, "Prima identificati, poi ripeti il comando: Password?"
+        )
+        msg = bot.register_next_step_handler(msg, process_password)
+    else:
+        return True
+
+
+def process_password(message):
+    chat_id = message.chat.id
+    text = message.text
+    if text == PASSWORD:
+        whiteList.append(chat_id)
+        with open("whitelist.txt", "a") as wlFile:
+            wlFile.write(str(chat_id) + "\n")
+        bot.send_message(chat_id, "ok")
+        whiteList.append(str(chat_id))
+    else:
+        bot.send_message(chat_id, "errore")
 
 
 def process_names(message):
     chat_id = message.chat.id
     try:
         text = message.text
-        names = text.replace(' ','').split(',')
+        names = text.replace(" ", "").split(",")
         tournament.setPlayers(names)
 
     except Exception as e:
         print(e)
-        msg = bot.send_message(chat_id, 'Inserimento non valido, scrivi il nome di ogni giocatore separato da una virgola')
+        msg = bot.send_message(
+            chat_id,
+            "Inserimento non valido, scrivi il nome di ogni giocatore separato da una virgola",
+        )
         bot.register_next_step_handler(msg, process_names)
     else:
-        msg = bot.send_message(chat_id, 'CHE IL TORNEO ABBIA INIZIO')
+        msg = bot.send_message(chat_id, "CHE IL TORNEO ABBIA INIZIO")
 
 
-@bot.message_handler(commands=['match'])
+@bot.message_handler(commands=["match"])
 def store_match(message):
     chat_id = message.chat.id
-    matchBuilder = MatchBuilder()
+    if checkPassword(chat_id):
+        matchBuilder = MatchBuilder()
 
-    #show all players' buttons
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    for name in tournament.getPlayerNames():
-        keyboard.add(types.KeyboardButton(name))
+        # show all players' buttons
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        for name in tournament.getPlayerNames():
+            keyboard.add(types.KeyboardButton(name))
 
-    msg = bot.send_message(chat_id, 'Chi è il primo giocatore?', reply_markup=keyboard)
-    bot.register_next_step_handler(msg, store_player1, matchBuilder)
-    
-def store_player1(message, matchBuilder : MatchBuilder):
+        msg = bot.send_message(
+            chat_id, "Chi è il primo giocatore?", reply_markup=keyboard
+        )
+        bot.register_next_step_handler(msg, store_player1, matchBuilder)
+
+
+def store_player1(message, matchBuilder: MatchBuilder):
     chat_id = message.chat.id
 
     try:
         matchBuilder.setPlayer1(tournament.getPlayerByName(message.text))
     except:
-        msg = bot.send_message(chat_id, 'Inserimento non valido, riprova')
+        msg = bot.send_message(chat_id, "Inserimento non valido, riprova")
         bot.register_next_step_handler(msg, store_player1, matchBuilder)
     else:
-        msg = bot.send_message(chat_id, 'Chi è il secondo giocatore?')
+        msg = bot.send_message(chat_id, "Chi è il secondo giocatore?")
         bot.register_next_step_handler(msg, store_player2, matchBuilder)
 
-def store_player2(message, matchBuilder : MatchBuilder):
+
+def store_player2(message, matchBuilder: MatchBuilder):
     chat_id = message.chat.id
     keyboard = types.ReplyKeyboardRemove(selective=False)
 
     try:
         matchBuilder.setPlayer2(tournament.getPlayerByName(message.text))
     except:
-        msg = bot.send_message(chat_id, 'Inserimento non valido, riprova')
+        msg = bot.send_message(chat_id, "Inserimento non valido, riprova")
         bot.register_next_step_handler(msg, store_player2, matchBuilder, keyboard)
     else:
-        msg = bot.send_message(chat_id, 'Quanti goal ha segnato ' + matchBuilder.p1.getName() + '?', reply_markup=keyboard)
+        msg = bot.send_message(
+            chat_id,
+            "Quanti goal ha segnato " + matchBuilder.p1.getName() + "?",
+            reply_markup=keyboard,
+        )
         bot.register_next_step_handler(msg, store_score_player1, matchBuilder)
 
-def store_score_player1(message, matchBuilder : MatchBuilder):
+
+def store_score_player1(message, matchBuilder: MatchBuilder):
     chat_id = message.chat.id
     try:
         matchBuilder.setScorePlayer1(int(message.text))
     except:
-        msg = bot.send_message(chat_id, 'Inserimento non valido, riprova')
+        msg = bot.send_message(chat_id, "Inserimento non valido, riprova")
         bot.register_next_step_handler(msg, store_score_player1, matchBuilder)
     else:
-        msg = bot.send_message(chat_id, 'Quanti goal ha segnato ' + matchBuilder.p2.getName() + '?')
+        msg = bot.send_message(
+            chat_id, "Quanti goal ha segnato " + matchBuilder.p2.getName() + "?"
+        )
         bot.register_next_step_handler(msg, store_score_player2, matchBuilder)
 
-def store_score_player2(message, matchBuilder : MatchBuilder):
+
+def store_score_player2(message, matchBuilder: MatchBuilder):
     chat_id = message.chat.id
     try:
         matchBuilder.setScorePlayer2(int(message.text))
     except:
-        msg = bot.send_message(chat_id, 'Inserimento non valido, riprova')
+        msg = bot.send_message(chat_id, "Inserimento non valido, riprova")
         bot.register_next_step_handler(msg, store_score_player2, matchBuilder)
     else:
         status = addMatchInTorunament(matchBuilder)
         if status:
-            msg = bot.send_message(chat_id, 'Match salvato')
+            msg = bot.send_message(chat_id, "Match salvato")
 
-def addMatchInTorunament(matchBuilder : MatchBuilder):
+
+def addMatchInTorunament(matchBuilder: MatchBuilder):
     try:
         tournament.addMatch(matchBuilder.build())
     except Exception as e:
@@ -105,43 +160,79 @@ def addMatchInTorunament(matchBuilder : MatchBuilder):
     else:
         return True
 
-@bot.message_handler(commands=['reset'])
+
+@bot.message_handler(commands=["matchesLeft"])
+def matchesLeft(message):
+    chat_id = message.chat.id
+    if checkPassword(chat_id):
+        combinations = itertools.combinations(tournament.getPlayerNames(), 2)
+        l = [' '.join(i) for i in combinations]
+        for m in tournament.getMatches():
+            for comb in l:
+                if m.getPlayer1().getName() in comb and m.getPlayer2().getName() in comb:
+                    l.remove(comb) 
+        msg = bot.send_message(
+            chat_id,  '\n'.join(l) if len(l)>0 else "No more matches"
+        )
+
+
+@bot.message_handler(commands=["reset"])
 def reset_tournament(message):
     chat_id = message.chat.id
-    tournament.reset()
-    msg = bot.send_message(chat_id, 'Torneo concluso')
+    if checkPassword(chat_id):
+        tournament.reset()
+        msg = bot.send_message(chat_id, "Torneo concluso")
 
-@bot.message_handler(commands=['ranking'])
+
+@bot.message_handler(commands=["ranking"])
 def show_rank(message):
     chat_id = message.chat.id
-    ranking = tournament.getRanking()
-    if len(ranking) == 0:
-        msg = bot.send_message(chat_id, 'Non ci sono dei match da giudicare')
-    else:
+    if checkPassword(chat_id):
+        ranking = tournament.getRanking()
+        if len(ranking) == 0:
+            msg = bot.send_message(chat_id, "Non ci sono dei match da giudicare")
+        else:
+            position = 1
+            ranking_str = "\n"
+            for player in ranking:
+                ranking_str += (
+                    str(position)
+                    + "\t"
+                    + str(player.name)
+                    + "\t"
+                    + str(player.score)
+                    + "\t"
+                    + str(player.getGoalDifference())
+                )
+                ranking_str += "\n"
+                position += 1
+            msg = bot.send_message(chat_id, ranking_str)
+
+
+@bot.message_handler(commands=["awards"])
+def awards(message):
+    chat_id = message.chat.id
+    if checkPassword(chat_id):
+        ranking = tournament.getRanking()
+
         position = 1
-        ranking_str = '\n'
+        ranking_str = "\n"
         for player in ranking:
-            ranking_str += str(position) + '\t' + str(player.name) + '\t' + str(player.score) + '\t' + str(player.getGoalDifference())
-            ranking_str += '\n'
+            ranking_str += (
+                str(position)
+                + "\t"
+                + str(player.name)
+                + "\t"
+                + str(player.score)
+                + "\t"
+                + str(player.getGoalDifference())
+            )
+            ranking_str += "\n"
             position += 1
         msg = bot.send_message(chat_id, ranking_str)
 
-@bot.message_handler(commands=['awards'])
-def awards(message):
-    chat_id = message.chat.id
-    ranking = tournament.getRanking()
-
-    position = 1
-    ranking_str = '\n'
-    for player in ranking:
-        ranking_str += str(position) + '\t' + str(player.name) + '\t' + str(player.score) + '\t' + str(player.getGoalDifference())
-        ranking_str += '\n'
-        position += 1
-    msg = bot.send_message(chat_id, ranking_str)
-
-    # no need to remember data
-    reset_tournament(message)
-
+        # no need to remember data
+        reset_tournament(message)
 
 
 bot.infinity_polling()
--- src/soccer/Match.py
+++ src/soccer/Match.py
@@ -3,18 +3,30 @@ from .Player import Player
 
 
 class Match:
-    def __init__(self, player1 : Player, player2 : Player, scorePlayer1, scorePlayer2):
+    def __init__(self, player1: Player, player2: Player, scorePlayer1, scorePlayer2):
         self.p1 = player1
         self.p2 = player2
         self.scorePlayer1 = scorePlayer1
         self.scorePlayer2 = scorePlayer2
 
-        self.p1.updateStatistics(goalScored = self.scorePlayer1, goalReceived = self.scorePlayer2)
-        self.p2.updateStatistics(goalScored = self.scorePlayer2, goalReceived = self.scorePlayer1)
+        self.p1.updateStatistics(
+            goalScored=self.scorePlayer1, goalReceived=self.scorePlayer2
+        )
+        self.p2.updateStatistics(
+            goalScored=self.scorePlayer2, goalReceived=self.scorePlayer1
+        )
         self.updateScore()
-    
+
     def __str__(self):
-        return str(self.player1.name) + ' ' + str(self.scorePlayer1) + ' - ' + str(self.scorePlayer2) + ' ' + str(self.player2.name)
+        return (
+            str(self.player1.name)
+            + " "
+            + str(self.scorePlayer1)
+            + " - "
+            + str(self.scorePlayer2)
+            + " "
+            + str(self.player2.name)
+        )
 
     def updateScore(self):
         if self.scorePlayer1 > self.scorePlayer2:
@@ -26,6 +38,12 @@ class Match:
         else:
             self.p1.updateScore(DRAW_POINTS)
             self.p2.updateScore(DRAW_POINTS)
+    
+    def getPlayer1(self):
+        return self.p1
+
+    def getPlayer2(self):
+        return self.p2
 
 
 class MatchBuilder:
@@ -35,18 +53,18 @@ class MatchBuilder:
         self.scorePlayer1 = None
         self.scorePlayer2 = None
 
-    def setPlayer1(self, player1 : Player) -> None:
+    def setPlayer1(self, player1: Player) -> None:
         self.p1 = player1
 
-    def setPlayer2(self, player2 : Player) -> None:
+    def setPlayer2(self, player2: Player) -> None:
         if self.p1.name == player2.name:
-            raise Exception(self.p1.name + ' non può scontrarsi con sè stesso')
+            raise Exception(self.p1.name + " non può scontrarsi con sè stesso")
         self.p2 = player2
 
-    def setScorePlayer1(self, scorePlayer1 : int) -> None:
+    def setScorePlayer1(self, scorePlayer1: int) -> None:
         self.scorePlayer1 = scorePlayer1
 
-    def setScorePlayer2(self, scorePlayer2 : int) -> None:
+    def setScorePlayer2(self, scorePlayer2: int) -> None:
         self.scorePlayer2 = scorePlayer2
 
     def build(self) -> Match:
